b888374e73d7150db408ee8f6b81e0015152d798
devmak <khan@nochallenge.net>
2024-04-07 21:44:23 +0600

    inkwell & gesture dectectot

--- lib/my_home_screen.dart
+++ lib/my_home_screen.dart
@@ -11,8 +11,35 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 156, 119, 221),
       appBar: AppBar(
-        title: const Text('Dice App'),
+        title: const Text(
+          'Dice App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+                child: InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/dice1.png'),
+              ),
+            )),
+            Expanded(
+                child: InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/dice2.png'),
+              ),
+            )),
+          ],
+        ),
       ),
     );
   }
